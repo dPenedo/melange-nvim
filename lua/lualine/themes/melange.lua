469b125d7b0e7adf9b617c8d629b42af6745a542
daniel <dpenedo@tutanota.com>
2024-09-28 09:59:11 -0300

    ajustes en dark y lualine

--- lua/lualine/themes/melange.lua
+++ lua/lualine/themes/melange.lua
@@ -6,45 +6,45 @@ local normal = palette.c
 local dark = palette.d
 
 return {
-    normal = {
-        a = { bg = grays.com, fg = grays.float, gui = "bold" },
-        b = { bg = grays.sel, fg = grays.com },
-        c = { bg = grays.float, fg = grays.com },
-        y = { bg = grays.ui, fg = grays.float },
-    },
-    insert = {
-        a = { bg = bright.red, fg = grays.float, gui = "bold" },
-        b = { bg = grays.sel, fg = normal.red },
-        c = { bg = grays.float, fg = grays.com },
-        y = { bg = normal.red, fg = grays.float },
-    },
-    command = {
-        a = { bg = bright.green, fg = grays.float, gui = "bold" },
-        b = { bg = grays.sel, fg = normal.green },
-        c = { bg = grays.float, fg = grays.com },
-        y = { bg = normal.green, fg = grays.float },
-    },
-    visual = {
-        a = { bg = bright.yellow, fg = grays.float, gui = "bold" },
-        b = { bg = grays.sel, fg = normal.yellow },
-        c = { bg = grays.float, fg = grays.com },
-        y = { bg = normal.yellow, fg = grays.float },
-    },
-    replace = {
-        a = { bg = bright.magenta, fg = grays.float, gui = "bold" },
-        b = { bg = grays.sel, fg = normal.magenta },
-        c = { bg = grays.float, fg = grays.com },
-        y = { bg = normal.magenta, fg = grays.float },
-    },
-    terminal = {
-        a = { bg = bright.red, fg = grays.float, gui = "bold" },
-        b = { bg = grays.sel, fg = normal.red },
-        c = { bg = grays.float, fg = grays.com },
-        y = { bg = normal.red, fg = grays.float },
-    },
-    inactive = {
-        a = { bg = grays.float, fg = grays.com },
-        b = { bg = grays.float, fg = grays.com },
-        c = { bg = grays.float, fg = grays.com },
-    },
+	normal = {
+		a = { bg = grays.com, fg = grays.float, gui = 'bold' },
+		b = { bg = grays.sel, fg = grays.com },
+		c = { bg = grays.float, fg = grays.com },
+		y = { bg = grays.sel, fg = grays.com },
+	},
+	insert = {
+		a = { bg = bright.red, fg = grays.float, gui = 'bold' },
+		b = { bg = grays.sel, fg = normal.red },
+		c = { bg = grays.float, fg = grays.com },
+		y = { bg = normal.red, fg = grays.float },
+	},
+	command = {
+		a = { bg = bright.green, fg = grays.float, gui = 'bold' },
+		b = { bg = grays.sel, fg = normal.green },
+		c = { bg = grays.float, fg = grays.com },
+		y = { bg = normal.green, fg = grays.float },
+	},
+	visual = {
+		a = { bg = bright.yellow, fg = grays.float, gui = 'bold' },
+		b = { bg = grays.sel, fg = normal.yellow },
+		c = { bg = grays.float, fg = grays.com },
+		y = { bg = normal.yellow, fg = grays.float },
+	},
+	replace = {
+		a = { bg = bright.magenta, fg = grays.float, gui = 'bold' },
+		b = { bg = grays.sel, fg = normal.magenta },
+		c = { bg = grays.float, fg = grays.com },
+		y = { bg = normal.magenta, fg = grays.float },
+	},
+	terminal = {
+		a = { bg = bright.red, fg = grays.float, gui = 'bold' },
+		b = { bg = grays.sel, fg = normal.red },
+		c = { bg = grays.float, fg = grays.com },
+		y = { bg = normal.red, fg = grays.float },
+	},
+	inactive = {
+		a = { bg = grays.float, fg = grays.com },
+		b = { bg = grays.float, fg = grays.com },
+		c = { bg = grays.float, fg = grays.com },
+	},
 }
